b386f5bd252079fc3424dd882b0d7934f52a8fa4
rladnfla <80800423+rladnfla@users.noreply.github.com>
2021-05-26 11:19:30 +0900

    .

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class GameManager : MonoBehaviour
+{
+    public static GameManager instance;
+    public bool isGameover = false; 
+    public Text scoreText; 
+    public GameObject gameoverUI; 
+    private int score = 0;
+
+    void Awake()
+    {
+        if (instance == null) 
+        { 
+            instance = this; 
+        } else 
+        {
+            Debug.LogWarning("두 개 이상의 게임 매니저가 존재합니다!"); 
+            Destroy(gameObject); 
+        }
+    }
+}
